0de01770ac141e88f097305a73ceccec0e4ab563
Mitchell Vogel <mjv58@cornell.edu>
2014-02-15 14:25:28 -0500

    mac woke up

--- server/PhoneRemote_Server/src/main/Mouse.java
+++ server/PhoneRemote_Server/src/main/Mouse.java
@@ -1,15 +1,27 @@
 package main;
 
 import java.awt.AWTException;
+import java.awt.Dimension;
 import java.awt.Robot;
+import java.awt.Toolkit;
+import java.util.LinkedList;
 
 public class Mouse {
 
 	double[][] co;
+	double dif0;
+	double dif1;
 	Robot r;
+	double height;
+	double width;
+	LinkedList<Double> smoothx;
+	LinkedList<Double> smoothy;
 	
 	public Mouse(double[][] canonicalOrientations) {
 		co = canonicalOrientations;
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		width = screenSize.getWidth();
+		height = screenSize.getHeight();
 		try {
 			r = new Robot();
 		} catch (AWTException e) {
@@ -19,8 +31,9 @@ public class Mouse {
 	}
 
 	public void updateOrientation(double[] orientation) {
-		// TODO Auto-generated method stub
-		
+		double x = width * (orientation[0] - co[1][0]) / (co[0][0] - co[1][0]);
+		double y = height * (orientation[1] - co[2][1]) / (co[3][1] - co[2][1]);
+		r.mouseMove((int) x, (int) y);
 	}
 
 }
